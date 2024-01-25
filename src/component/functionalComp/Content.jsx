00dea7f3c5c459a8d1f5eee43018ad8a9ce83867
sasi <sasijagan03@email.com>
2024-01-25 22:49:23 +0530

    -a

--- src/component/functionalComp/Content.jsx
+++ src/component/functionalComp/Content.jsx
@@ -4,7 +4,7 @@ const Content = () => {
         <div><br />
             <h1>Welcome To React App</h1>
             <p style={{color:"green"}}>React is a free and open-source front-end Javascript library for building user interfaces based on components</p>
-            <br/>
+            
             <p style={{color:"green"}}>Components come in two types , Class components and Functional components</p>
         </div>
     )
